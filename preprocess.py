5f8caa8cae467e6f0816beab4c9e39f33cf3c81c
Brett Hannigan <hanniganbrett@gmail.com>
2019-03-19 23:30:34 -0700

    Count number of atoms in reactants, products.

--- preprocess.py
+++ preprocess.py
@@ -1,13 +1,32 @@
-import numpy as np
-import csv
 import sys
-from rdkit import Chem
-from rdkit.Chem import AllChem
+print(sys.path)
+import csv
+
+import numpy as np
+#import rdkit
+from rdkit.Chem import AllChem as Chem
+
+NUM_RXNS_TO_READ = 10000
 
 with open("data/1976_Sep2016_USPTOgrants_smiles.rsmi", newline='') as f:
     csv_reader = csv.reader(f, delimiter='\t')
     next(csv_reader)
+    line_count = 0
+    reactions = []
     for row in csv_reader:
-        print('a')
-        rxn = AllChem.ReactionFromSmarts(row[0].split(None, 1)[0])
-        
+        reactions.append(Chem.ReactionFromSmarts(row[0].split(None, 1)[0]))
+        line_count += 1
+        if line_count == NUM_RXNS_TO_READ:
+            break
+
+num_reactant_atoms = np.zeros(len(reactions), dtype=np.int32)
+num_product_atoms = np.zeros(len(reactions), dtype=np.int32)
+for i, rxn in enumerate(reactions):
+    reac = Chem.ChemicalReaction.GetReactants(rxn)
+    prod = Chem.ChemicalReaction.GetProducts(rxn)
+    for r in reac:
+        num_reactant_atoms[i] += r.GetNumAtoms()
+    for p in prod:
+        num_product_atoms[i] += p.GetNumAtoms()
+
+x = 5
