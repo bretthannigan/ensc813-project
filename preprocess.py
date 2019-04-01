a4ea0472afc8d02260483f2865867ea2fe7a1fb4
Brett Hannigan <hanniganbrett@gmail.com>
2019-04-01 07:06:26 -0700

    LogP prediction.

--- preprocess.py
+++ preprocess.py
@@ -9,65 +9,79 @@ import scipy.sparse as sp
 
 import ReactionGraph as rg
 
-# Import
-DATA_FILE = "data/1976_Sep2016_USPTOgrants_smiles.rsmi"
-NUM_RXNS_TO_READ = 1000
-MAX_NUM_ATOMS = 128
-# Test/validation
-TRAIN_FRACTION = 0.8
+def import_patent_dataset(data_file="data/1976_Sep2016_USPTOgrants_smiles.rsmi", num_reactions=np.Inf, max_num_atoms=128):
 
-with open(DATA_FILE, newline='') as f:
-    csv_reader = csv.reader(f, delimiter='\t')
-    next(csv_reader)
-    reactant = []
-    product = []
-    try_count = 0
-    success_count = 0
-    fail_count = 0
-    is_error_on_iter = False
-    for row in csv_reader:
-        fail_count += is_error_on_iter
+    with open(data_file, newline='') as f:
+        csv_reader = csv.reader(f, delimiter='\t')
+        next(csv_reader)
+        reactant = []
+        product = []
+        try_count = 0
+        success_count = 0
+        fail_count = 0
         is_error_on_iter = False
-        try_count += 1
-        row = row[0].split(None, 1)[0]
-        # Reaction SMILES format: reactant_1.reactant_2>reagent>product_1.product_2
-        reac, _, prod = row.split(">") # Not interested in reagents/catalysts.
-        reac = Chem.MolFromSmiles(reac)
-        try:
-            Chem.SanitizeMol(reac)
-            num_reactant_atoms = Chem.Mol.GetNumAtoms(reac)
-        except:
-            is_error_on_iter = True
-            print("\tSkipping reaction on line {}, error in reactant SMILES.".format(try_count + 1))
-            continue
-        prod = Chem.MolFromSmiles(prod)
-        try:
-            Chem.SanitizeMol(prod)
-            num_product_atoms = Chem.Mol.GetNumAtoms(prod)
-        except:
-            is_error_on_iter = True
-            print("\tSkipping reaction on line {}, error in product SMILES".format(try_count + 1))
-            continue
-        if num_reactant_atoms>MAX_NUM_ATOMS or num_product_atoms>MAX_NUM_ATOMS:
-            is_error_on_iter = True
-            print("\tSkipping reaction on line {}, has greater than {} atoms.".format(try_count + 1, MAX_NUM_ATOMS))
-            continue
-        reactant.append(rg.ReactionSideGraph.from_rdMol([reac], MAX_NUM_ATOMS))
-        product.append(rg.ReactionSideGraph.from_rdMol([prod], MAX_NUM_ATOMS))
-        success_count += 1
-        if success_count >= NUM_RXNS_TO_READ:
-            break
+        for row in csv_reader:
+            fail_count += is_error_on_iter
+            is_error_on_iter = False
+            try_count += 1
+            row = row[0].split(None, 1)[0]
+            # Reaction SMILES format: reactant_1.reactant_2>reagent>product_1.product_2
+            reac, _, prod = row.split(">") # Not interested in reagents/catalysts.
+            reac = Chem.MolFromSmiles(reac)
+            try:
+                Chem.SanitizeMol(reac)
+                num_reactant_atoms = Chem.Mol.GetNumAtoms(reac)
+            except:
+                is_error_on_iter = True
+                print("\tSkipping reaction on line {}, error in reactant SMILES.".format(try_count + 1))
+                continue
+            prod = Chem.MolFromSmiles(prod)
+            try:
+                Chem.SanitizeMol(prod)
+                num_product_atoms = Chem.Mol.GetNumAtoms(prod)
+            except:
+                is_error_on_iter = True
+                print("\tSkipping reaction on line {}, error in product SMILES".format(try_count + 1))
+                continue
+            if num_reactant_atoms>max_num_atoms or num_product_atoms>max_num_atoms:
+                is_error_on_iter = True
+                print("\tSkipping reaction on line {}, has greater than {} atoms.".format(try_count + 1, max_num_atoms))
+                continue
+            reactant.append(rg.ReactionSideGraph.from_rdMol([reac], max_num_atoms))
+            product.append(rg.ReactionSideGraph.from_rdMol([prod], max_num_atoms))
+            success_count += 1
+            if success_count >= num_reactions:
+                break
+        print("Attempted import of {} reactions with {} successes and {} skips.".format(try_count, success_count, fail_count))
+
+    with open("/data/LoweUSPTOGrants_1976-2016_{}Atoms_{}Reactions.pickle".format(max_num_atoms, success_count), 'wb') as f:
+        pickle.dump(reactant, f)
+        pickle.dump(product, f)
+
+def import_logp_dataset(data_file="data/logP_dataset.csv", num_compounds=100000, max_num_atoms=32):
+    with open(data_file) as f:
+        csv_reader = csv.reader(f, delimiter=',')
+        compound = []
+        log_p = []
+        try_count = 0
+        success_count = 0
+        fail_count = 0
+        for row in csv_reader:
+            try_count += 1
+            new_compound = Chem.MolFromSmiles(row[0])
+            try:
+                Chem.SanitizeMol(new_compound)
+            except:
+                fail_count += 1
+                print("\tSkipping compound on line {}, error in SMILES".format(try_count))
+                continue
+            compound.append(rg.ReactionSideGraph.from_rdMol([new_compound], max_num_atoms))
+            log_p.append(float(row[1]))
+            success_count += 1
     print("Attempted import of {} reactions with {} successes and {} skips.".format(try_count, success_count, fail_count))
 
-with open("/data/LoweUSPTOGrants_1976-2016_{}Atoms_{}Reactions.pickle".format(MAX_NUM_ATOMS, NUM_RXNS_TO_READ), 'wb') as f:
-    pickle.dump(reactant, f)
-    pickle.dump(product, f)
+    with open("KagglelogP.pickle", 'wb') as f:
+        pickle.dump(compound, f)
+        pickle.dump(log_p, f)
 
-A = [r.get_adjacency(normalize=True) for r in reactant]
-X = [r.get_features() for r in reactant]
-y = [p.get_adjacency(normalize=True) for p in product]
-X_y = [p.get_features() for p in product]
-
-
-
-x = 5
+import_logp_dataset()
--- train_gcn.py
+++ train_gcn.py
@@ -2,34 +2,35 @@ from __future__ import print_function
 
 import os
 import sys
-sys.path.append(os.path.abspath('./relational-gcn/'))
+sys.path.append(os.path.abspath('./keras-gcn/'))
+sys.path.append(os.path.abspath('./keras-gcn/tests'))
 
 import numpy as np
 import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 
-from keras.layers import Input, Dropout
+from keras.layers import Input, Dropout, Concatenate, Dense, GlobalAveragePooling1D, GlobalMaxPooling1D
 from keras.models import Model
 from keras.optimizers import Adam
 from keras.regularizers import l1, l2
 from sklearn.model_selection import train_test_split
 
-from kegra.layers.graph import GraphConvolution
-from kegra.utils import *
+from keras_gcn import GraphConv, GraphMaxPool, GraphAveragePool
 
 import pickle as pkl
 
 import time
+import argparse
 
 from ReactionData import DataGenerator
 
 np.random.seed()
 
 ap = argparse.ArgumentParser()
-ap.add_argument("-d", "--dataset", type=str, default="LoweUSPTOGrants_1976-2016_128Atoms_1000Reactions.pickle",
+ap.add_argument("-d", "--dataset", type=str, default="KagglelogP.pickle",
                 help="Dataset string ('aifb', 'mutag', 'bgs', 'am')")
-ap.add_argument("-e", "--epochs", type=int, default=500,
+ap.add_argument("-e", "--epochs", type=int, default=50,
                 help="Number training epochs")
 ap.add_argument("-hd", "--hidden", type=int, default=256,
                 help="Number hidden units")
@@ -37,7 +38,7 @@ ap.add_argument("-do", "--dropout", type=float, default=0.,
                 help="Dropout rate")
 ap.add_argument("-b", "--bases", type=int, default=-1,
                 help="Number of bases used (-1: all)")
-ap.add_argument("-lr", "--learnrate", type=float, default=0.01,
+ap.add_argument("-lr", "--learnrate", type=float, default=0.1,
                 help="Learning rate")
 ap.add_argument("-l2", "--l2norm", type=float, default=0.,
                 help="L2 normalization of input weights")
@@ -63,58 +64,73 @@ BASES = args['bases']
 DO = args['dropout']
 TRAIN_FRACTION = args['trainfrac']
 
+# with open(DATASET, 'rb') as f:
+#     reactant = pkl.load(f)
+#     %product = pkl.load(f)
+
+# A = [r.get_adjacency(normalize=True) for r in reactant]
+# X = [r.get_features() for r in reactant]
+# y = [p.get_adjacency(normalize=True) for p in product]
+# X_y = [p.get_features() for p in product]
+
 with open(DATASET, 'rb') as f:
-    reactant = pkl.load(f)
-    product = pkl.load(f)
+    compound = pkl.load(f)
+    log_p = pkl.load(f)
 
-A = [r.get_adjacency(normalize=True) for r in reactant]
-X = [r.get_features() for r in reactant]
-y = [p.get_adjacency(normalize=True) for p in product]
-X_y = [p.get_features() for p in product]
+A = [c.get_adjacency(normalize=True) for c in compound]
+X = [c.get_features() for c in compound]
+y = np.asarray(log_p)
 
-A_train, A_test, X_train, X_test, y_train, y_test, X_y_train, X_y_test = train_test_split(A, X, y, X_y, train_size=TRAIN_FRACTION, random_state=0, shuffle=True)
+A_train, A_test, X_train, X_test, y_train, y_test = train_test_split(A, X, y, train_size=TRAIN_FRACTION, random_state=0, shuffle=True)
 
 num_nodes = A[0].shape[0]
 support = 1
 
-A_in = [InputAdj(sparse=True)]
-
-training_data_generator = DataGenerator(A_train, X_train, y_train, X_y_train, batch_size=128)
-validation_data_generator = DataGenerator(A_test, X_test, y_test, X_y_test, batch_size=128)
+# training_data_generator = DataGenerator(A_train, X_train, y_train, X_y_train, batch_size=128)
+# validation_data_generator = DataGenerator(A_test, X_test, y_test, X_y_test, batch_size=128)
 
 # Define model architecture
-X_in = Input(shape=(X[0].shape[1],))
-H = Dropout(DO)(X_in)
-# H = GraphConvolution(1024, support, num_bases=BASES, featureless=False, bias=True,
-#                      activation='relu')([H] + A_in)#,
-#                      #W_regularizer=l1(L2))([H] + A_in)
+
+X_in = Input(shape=(None, X_train[0].shape[1]), name='Input-Features')
+A_in = Input(shape=(None, A_train[0].shape[1]), sparse=False, name='Input-Adjacency')
+H = GraphConv(units=1024, step_num=1, name='GraphConv-1',activation='relu')([X_in, A_in])
 #H = Dropout(DO)(H)
-H = GraphConvolution(10000, support, num_bases=BASES, featureless=False, bias=True,
-                     activation='relu')([H] + A_in)
-# H = GraphConvolution(1024, support, num_bases=BASES, featureless=False, bias=True,
-#                      activation='relu')([H] + A_in)
-# H = GraphConvolution(1024, support, num_bases=BASES, featureless=False, bias=True,
-#                      activation='relu')([H] + A_in)
-# #H = Dropout(DO)(H)
-# H = GraphConvolution(1024, support, num_bases=BASES, featureless=False, bias=True,
-#                      activation='relu')([H] + A_in)
+H = GraphConv(units=1024, step_num=1, name='GraphConv-2',activation='relu')([H, A_in])
 #H = Dropout(DO)(H)
-Y = GraphConvolution(y_train[0].shape[1] + X_y_train[0].shape[1], support, num_bases=BASES,
-                     activation='linear')([H] + A_in)
+#H = Concatenate(axis=2)([X_in, A_in])
+H = Dense(units=512, activation='tanh', name='Dense-1')(H)
+H = Dense(units=256, activation='tanh', name='Dense-2')(H)
+H = Dense(units=1, activation='linear', name='Dense-3')(H)
+# Y = Dense(units=146, activation='linear')(H)
+# H = GraphConv(units=1, step_num=1, name='GraphConv-3',activation='relu')([H, A_in])
+Y = GlobalAveragePooling1D(data_format='channels_last', name='Global-Pooling')(H)
+#Y = GraphConv(units=146, step_num=1, name='GraphConv-3',activation='linear')([H, A_in])
 
 # Compile model
-model = Model(inputs=[X_in] + A_in, outputs=Y)
-model.compile(loss='mean_squared_error', optimizer=Adam(lr=LR))
+model = Model(inputs=[X_in, A_in], outputs=Y)
+model.compile(loss='mse', optimizer=Adam(lr=LR))
 model.summary()
-tr = [sp.vstack(X_train).todense(), sp.vstack(A_train)]
-yd = sp.vstack(y_train)
-tr = [X_train[0].todense(), A_train[0]]
-yd = np.concatenate((X_y_train[0].todense(), y_train[0].todense()), axis=1)
-#tr = sp.vstack(([sp.hstack((x.todense(), a)) for x, a in zip(X_train, A_train)]))
+
+# tr = [np.expand_dims(X_train[1].todense(), 0), np.expand_dims(A_train[1].todense(), 0)]
+# yd = np.expand_dims(np.concatenate((X_y_train[1].todense(), y_train[1].todense()), axis=1), 0)
+
+x_t = [np.asarray(x.todense()) for x in X_train]
+a = [np.asarray(a.todense()) for a in A_train]
+x_t_val = [np.asarray(x.todense()) for x in X_test]
+a_val = [np.asarray(a.todense()) for a in A_test]
+# x_y = [np.asarray(x_y.todense()) for x_y in X_y_train]
+# y = [np.asarray(y.todense()) for y in y_train]
+
+tr = [np.stack(x_t, axis=0), np.stack(a, axis=0)]
+val = [np.stack(x_t_val, axis=0), np.stack(a_val, axis=0)]
+# yd = np.concatenate((np.stack(x_y, axis=0), np.stack(y, axis=0)), axis=2)
+yd = y_train
+yd_val = y_test
+
 preds = None
-#model.fit([np.concatenate((X_train[0].todense(), X_train[1].todense()), axis=0), sp.vstack((A_train[0], A_train[1]))], sp.vstack((y_train[0], y_train[1])), epochs=50, verbose=2, batch_size=128)
-model.fit(tr, yd, epochs=NB_EPOCH, verbose=2, batch_size=1)
-preds = model.predict(tr, batch_size=128)
+
+history = model.fit(tr, yd, epochs=NB_EPOCH, verbose=2, batch_size=1, validation_data=(val, yd_val))
+preds = model.predict(tr, batch_size=1)
 
 plt.subplot(3, 1, 1)
 plt.matshow(tr[0][0:40,0:28],cmap=plt.cm.Blues)
